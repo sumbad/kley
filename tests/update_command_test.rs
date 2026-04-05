afcab1c4d6cbf9bd1b68005df0324cca255c52db
Denis Sumbaev <sumbad@ya.ru>
2026-04-05 14:53:27 +0800

    review: use tmp dir as a test's HOME dir

--- tests/update_command_test.rs
+++ tests/update_command_test.rs
@@ -39,22 +39,24 @@ use common::{setup_kley_and_project, setup_package_json};
 fn test_update_single_package_success() -> Result<(), Box<dyn std::error::Error>> {
     // 1. Setup
     let temp_dir = tempdir()?;
-    let temp_path = temp_dir.path();
+    let home_dir = temp_dir.path();
 
     // Create a source project to be published
-    let source_project_path = temp_path.join("source_project");
+    let source_project_path = home_dir.join("source_project");
     fs::create_dir_all(&source_project_path)?;
     setup_package_json(&source_project_path, "source_project", "1.0.0")?;
     let source_file_path = source_project_path.join("index.js");
     fs::write(&source_file_path, "console.log('v1.0.0');")?;
 
     // Create a target project that will consume the source project
-    let target_project_path = temp_path.join("target_project");
+    let target_project_path = home_dir.join("target_project");
     setup_kley_and_project(&target_project_path, "target_project", "1.0.0")?;
 
     // 2. Publish the initial version of the source project
     let mut cmd = Command::cargo_bin("kley")?;
-    cmd.arg("publish").current_dir(&source_project_path);
+    cmd.arg("publish")
+        .current_dir(&source_project_path)
+        .env("HOME", home_dir);
     cmd.assert().success().stdout(predicate::str::contains(
         "Package successfully published to store!",
     ));
@@ -63,7 +65,8 @@ fn test_update_single_package_success() -> Result<(), Box<dyn std::error::Error>
     let mut cmd = Command::cargo_bin("kley")?;
     cmd.arg("add")
         .arg("source_project")
-        .current_dir(&target_project_path);
+        .current_dir(&target_project_path)
+        .env("HOME", home_dir);
     cmd.assert()
         .success()
         .stdout(predicate::str::contains("package.json has been updated!"));
@@ -78,7 +81,9 @@ fn test_update_single_package_success() -> Result<(), Box<dyn std::error::Error>
 
     // 5. Re-publish the updated source project
     let mut cmd = Command::cargo_bin("kley")?;
-    cmd.arg("publish").current_dir(&source_project_path);
+    cmd.arg("publish")
+        .current_dir(&source_project_path)
+        .env("HOME", home_dir);
     cmd.assert().success().stdout(predicate::str::contains(
         "Package successfully published to store!",
     ));
@@ -87,7 +92,8 @@ fn test_update_single_package_success() -> Result<(), Box<dyn std::error::Error>
     let mut cmd = Command::cargo_bin("kley")?;
     cmd.arg("update")
         .arg("source_project")
-        .current_dir(&target_project_path);
+        .current_dir(&target_project_path)
+        .env("HOME", home_dir);
     cmd.assert()
         .success()
         .stdout(predicate::str::contains("Updated source_project"));
