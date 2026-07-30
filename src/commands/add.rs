7b6b2c244458c526cd1376e1ec046784dc7a2aea
Denis Sumbaev <sumbad@ya.ru>
2026-07-31 00:08:04 +0800

    lint: format and lint code

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -8,7 +8,12 @@ use crate::registry::Registry;
 use crate::utils::{self, work_dirs};
 
 /// Add logic
-pub fn add(registry: &mut Registry, package_name_version: &str, is_dev: bool, pure: bool) -> Result<()> {
+pub fn add(
+    registry: &mut Registry,
+    package_name_version: &str,
+    is_dev: bool,
+    pure: bool,
+) -> Result<()> {
     let (package_name, package_version) = utils::package_name_version_parse(package_name_version);
 
     utils::validate_version_in_registry(registry, package_name, package_version);
@@ -18,7 +23,12 @@ pub fn add(registry: &mut Registry, package_name_version: &str, is_dev: bool, pu
     run_update(registry, package_name, &std::env::current_dir()?)?;
 
     if pure {
-        println!("{}", "Note. package.json and node_modules left untouched (--pure).".italic().bright_black());
+        println!(
+            "{}",
+            "Note: package.json and node_modules left untouched (--pure)."
+                .italic()
+                .bright_black()
+        );
     } else {
         // --- Automate package.json modification ---
         PackageJson::update_dependency(&dirs.project_dir, package_name, is_dev)?;
